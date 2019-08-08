e9287c69547f091cd2d15f09117cb5486eb26797
QuocDat1994 <quocdat1994@gmail.com>
2019-08-08 10:49:23 +0700

    update information

--- scripts/projectData.js
+++ scripts/projectData.js
@@ -1,13 +1,4 @@
 const projectData = {
-  comicoIndo: {
-    name: "Comico Indonesia",
-    type: "Web Application",
-    image: "https://i.ibb.co/jkcbqZY/comico-Indo.jpg",
-    company: "NHN Vietnam",
-    link: "http://www.comico.co.id/",
-    description:
-      "COMICO is a webtoon platform from Japan and now also established its presence in Indonesia.<br /><br /> Comico offers free-to-read as well as premium comics from various genres."
-  },
   comicoThai: {
     name: "Comico Thailand",
     type: "Web Application",
@@ -15,7 +6,16 @@ const projectData = {
     company: "NHN Vietnam",
     link: "http://www.comico.in.th/",
     description:
-      "COMICO is a webtoon platform from Japan and now also established its presence in Indonesia.<br /><br /> Comico offers free-to-read as well as premium comics from various genres."
+      "COMICO is a webtoon platform from Japan and now also established its presence in Thailand. Comico offers free-to-read as well as premium comics from various genres.<br /><br /> My main task in this project is maintaining the website of Comico Thailand. Fixing bugs that are reported by users and the operation team."
+  },
+  comicoIndo: {
+    name: "Comico Indonesia",
+    type: "Web Application",
+    image: "https://i.ibb.co/jkcbqZY/comico-Indo.jpg",
+    company: "NHN Vietnam",
+    link: "http://www.comico.co.id/",
+    description:
+      "COMICO Indonesia is also a webtoon platform. This project is cloned from Comico Thailand, so they are mostly similar in terms of template design. <br /><br /> My main task is also maintaining the website of Comico Indonesia. However, since it is cloned from a different project, there are a lot of unused code as well as bugs to remove. <br /><br /> On the other hand, I am also responsible for adding new features to improve the User Experience and make Comico Indonesia distinct from Comico Thailand."
   },
   cheerUpKPI: {
     name: "Cheer Up KPI",
@@ -24,7 +24,7 @@ const projectData = {
     company: "Persol Process & Technology",
     link: "https://appadvice.com/app/cheer-up-kpi/1035541546",
     description:
-      "COMICO is a webtoon platform from Japan and now also established its presence in Indonesia.<br /><br /> Comico offers free-to-read as well as premium comics from various genres."
+      "Cheer Up KPI is a business application for recruitment agencies. It is used to measure how well companies or individuals are performing.<br /><br /> It has a web version and an Android version. I worked in a team of 3 people to develop the Android version of Cheer Up KPI, using Android Studio."
   },
   soul: {
     name: "SouL Magazine",
@@ -33,7 +33,7 @@ const projectData = {
     company: "SENO Ltd.",
     link: "",
     description:
-      "COMICO is a webtoon platform from Japan and now also established its presence in Indonesia.<br /><br /> Comico offers free-to-read as well as premium comics from various genres."
+      "SouL Magazine is a product of the startup company SENO. It is a magazine about Japanese 2D culture, which includes game, manga, anime, etc. It is officially published in Vietnam. The latest issue is Vol.36. <br /><br /> I was in charge of writing and editing magazine articles. Planning new content for each issue. Non-IT related job."
   },
   smackjeeves: {
     name: "Smackjeeves",
@@ -42,7 +42,7 @@ const projectData = {
     company: "NHN Vietnam",
     link: "",
     description:
-      "COMICO is a webtoon platform from Japan and now also established its presence in Indonesia.<br /><br /> Comico offers free-to-read as well as premium comics from various genres."
+      "Smack Jeeves is a free webcomic hosting site. Webcomic creators can create an account to post their webcomic. It was written in PHP and now is very slow. Therefore, the company wants to update it using other language and frameworks. <br/><br/> I am in charge of making UI for Admin Panel, using Bootstraps and jQuery libraries."
   }
 };
 
